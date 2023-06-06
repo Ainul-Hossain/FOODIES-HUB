e90cf33e5f334d6e3a5a20748fb8b659517973fe
ainul-hossain <ainulsakibs@gmail.com>
2023-06-07 01:54:24 +0600

    Navbar done!

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(){
+    return (
+        <div className="navbar container">
+            <a href="#" className="logo">F<span className="logo-color">oo</span>diesHub</a>
+
+            <div className="nav-links">
+                <a href="#">Home</a>
+                <a href="#">Recipes</a>
+                <a href="#">Settings</a>
+            </div>
+        </div>
+    )
+}
